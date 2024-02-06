38b1ccf9afbbaec362a52641bd4a1c81f1c22b67
naveenworkm <nk921038@gmail.com>
2024-02-06 18:55:04 +0530

    added schooling entity with tests

---
+++ Server/CDAC_Project_WebBasedRecruitmentSystem/src/main/java/com/app/repository/SchoolingEntityRepository.java
@@ -0,0 +1,9 @@
+package com.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.entities.SchoolingEntity;
+
+public interface SchoolingEntityRepository extends JpaRepository<SchoolingEntity, Long> {
+
+}
---
+++ Server/CDAC_Project_WebBasedRecruitmentSystem/src/test/java/com/app/repository/SchoolingEntityRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.app.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.app.entities.ApplicantEntity;
+import com.app.entities.EducationEntity;
+import com.app.entities.SchoolingEntity;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class SchoolingEntityRepositoryTest {
+	
+
+	@Autowired
+	private ApplicantRepository applicantRepo;
+	
+	@Autowired
+	private SchoolingEntityRepository schoolingRepo;
+	
+	@Test
+	void testAddApplicants() {
+		
+		ApplicantEntity applicant1= applicantRepo.findById(1l).orElseThrow();
+		ApplicantEntity applicant2= applicantRepo.findById(2l).orElseThrow();
+		ApplicantEntity applicant3= applicantRepo.findById(3l).orElseThrow();
+		ApplicantEntity applicant4= applicantRepo.findById(4l).orElseThrow();
+		
+		List<SchoolingEntity> list = List.of(
+				new SchoolingEntity(applicant1,"CBSE",LocalDate.of(2014, 8, 18),62,"CBSE",LocalDate.of(2016, 8, 18),68),
+				new SchoolingEntity(applicant2,"DBS",LocalDate.of(2011, 8, 18),92,"DBS",LocalDate.of(2013, 8, 18),82),
+				new SchoolingEntity(applicant3,"MDU",LocalDate.of(2016, 8, 18),72,"MDU",LocalDate.of(2018, 8, 18),70),
+				new SchoolingEntity(applicant4,"ICSE",LocalDate.of(2010, 8, 18),52,"ICSE",LocalDate.of(2012, 8, 18),58)
+				);
+		List<SchoolingEntity> list2 = schoolingRepo.saveAll(list);
+		assertEquals(4, list2.size());
+
+	}
+	
+}
