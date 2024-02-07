be1789b3bc1acaac5070e7e7b01a46af389c17c6
kd3-80115 <amkadnan89@gmail.com>
2024-02-07 17:43:01 +0530

    fix applicantEntity

--- Server/CDAC_Project_WebBasedRecruitmentSystem/src/main/java/com/app/entities/ApplicantEntity.java
+++ Server/CDAC_Project_WebBasedRecruitmentSystem/src/main/java/com/app/entities/ApplicantEntity.java
@@ -1,11 +1,7 @@
 package com.app.entities;
 
 
-import java.util.ArrayList;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -13,12 +9,11 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
