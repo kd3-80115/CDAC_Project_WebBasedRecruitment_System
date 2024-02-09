98daca5314a07776983e9cb5dfb98f53c7915eb0
kd3-80115 <amkadnan89@gmail.com>
2024-02-09 12:11:51 +0530

    complete admin controller and end point

--- Server/CDAC_Project_WebBasedRecruitmentSystem/src/main/java/com/app/service/AdminService.java
+++ Server/CDAC_Project_WebBasedRecruitmentSystem/src/main/java/com/app/service/AdminService.java
@@ -1,8 +1,9 @@
 package com.app.service;
 
-import java.util.List;import com.app.entities.JobInfoEntity;
+import java.util.List;
 import com.app.payload.request.HrRegistrationDetailsRequest;
 import com.app.payload.response.AnalysisResponseAdmin;
+import com.app.payload.response.ApiResponse;
 import com.app.payload.response.HrDetailsResponse;
 import com.app.payload.response.JobDetailsResponse;
 
@@ -10,7 +11,7 @@ public interface AdminService {
 
 	
 	//register hr
-	public String registerHr(HrRegistrationDetailsRequest hr);
+	public ApiResponse registerHr(HrRegistrationDetailsRequest hr);
 	
 	
 	//get list of all hr
@@ -19,14 +20,13 @@ public interface AdminService {
 	//get list of all jobs
 	public List<JobDetailsResponse> getAllJobs();
 	
-	//delete hr
-	public String deleteHrById(Long hrId);
+	
 	
 	//deactivate hr
-	public String deactivateHrById(Long hrId);
+	public ApiResponse deactivateHrById(Long hrId);
 	
 	//get no of {total hr,activehr,total jobs,openjobs,}
-	public List<AnalysisResponseAdmin> getReport();
+	public AnalysisResponseAdmin getReport();
 	
 	
 }
